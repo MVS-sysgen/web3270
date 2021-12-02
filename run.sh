0af4661b8e446eee088aeb3a886e646f8e1ed41b
mainframed <mainframed767@gmail.com>
2021-12-01 16:24:40 -0800

    fix typo

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Docker is unable to run python on its own for some reason
-# This scrip is needed to run properly
+# This script is needed to run properly
 python3 -u /web3270/server.py --config /config --certs /certs
